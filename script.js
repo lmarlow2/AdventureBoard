82aeea3da4a14369c957c1df7300672e67f8eca3
lmarlow2 <47681639+lmarlow2@users.noreply.github.com>
2019-04-10 22:20:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function newJob(){
-  document.getElementById("dat").innerHTML = '<form><label for="name"><b>Name</b></label><input type="text" placeholder="Enter Name" name="name" required><div class="center"><label for="description"><b>Job Description</b></label><textarea rows="20" style="width: 100%" required>Description of the job to be completed; rewards; etc...</textarea><button type="submit" style="width: 100%">Submit Job</button></div></form>';
+  document.getElementById("dat").innerHTML = '<form><label for="name"><b>Name</b></label><input type="text" placeholder="Enter Name" name="name" required><div class="center"><label for="description"><b>Job Description</b></label><textarea rows="20" style="width: 100%" required>Description of the job to be completed; rewards; etc...</textarea><button type="submit" style="width: 100%" onclick"submitJob()">Submit Job</button></div></form>';
 }
 
 function submitJob(){
-  
+  document.getElementById("dat").innerHTML = '<button class="bigButton" type="button" style="float: left;" onclick="newJob()">Submit New Job</button><button class="bigButton" type="button" style="float: right;" onclick="viewJobs()">Bid On Jobs</button>';
 }
 
 function viewJobs(){
